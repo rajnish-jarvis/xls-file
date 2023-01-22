71a6fd4cc078f91a06402248ba6f5be3552a0628
Rajnish mishra <rajnish.mishra@jarvis.consulting>
2023-01-22 10:02:44 +0530

    Source code uri updated

--- lib/xls/file.rb
+++ lib/xls/file.rb
@@ -4,6 +4,9 @@ require_relative "file/version"
 
 module Xls
   module File
+    def self.call(name)
+      puts "Hello, #{name}! I'm Ruby!"
+    end
     class Error < StandardError; end
     # Your code goes here...
   end
