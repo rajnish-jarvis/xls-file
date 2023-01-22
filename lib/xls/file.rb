d219dddc7bb023b145842b3e9f39eab0b1b888cc
Rajnish mishra <rajnish.mishra@jarvis.consulting>
2023-01-22 13:02:11 +0530

    Gem testing

--- lib/xls/file.rb
+++ lib/xls/file.rb
@@ -3,30 +3,28 @@
 require_relative "file/version"
 require 'caxlsx'
 module Xls
-    def self.generate_xls
+    def self.generate_xls(data, headers)
       package = Axlsx::Package.new
       work_sheet = package.workbook.add_worksheet(name: 'work_book')
-      headers = ["Name", "Phone Number"]
-      data = [{name: 'Rajnish Mishra', phone: "7979097455"}]
       work_sheet.add_row headers
+      unless data.is_a? Array
+        data = data.as_json
+      end
       data.each do |row|
         formatted_row = formatted_excel_data(row)
         work_sheet.add_row formatted_row
       end
-      filename = "Test- #{Date.today.strftime("%dth %b %Y")}"
+      filename = "xls-file-#{Date.today.strftime("%dth %b %Y")}"
       path = "tmp/#{filename}"
       package.use_shared_strings = true
       package.serialize(path)
       return path
     end
     def self.formatted_excel_data(row)
-      [
-        row[:name],
-        row[:phone]
-      ]
+      row.collect{|k, v| [v]}.flatten!
     end
-    def self.call(name)
-      puts "Hello, #{name}! I'm Ruby!"
+    def self.test
+      puts "Cheers!! xls-file installed successfully"
     end
     class Error < StandardError; end
     # Your code goes here...
