8a4d0446ba01ae9f65dfb1e31d1ac42ce98283dc
rajnishmishra <rajnish.mishra@jarvis.consulting>
2025-04-22 19:30:59 +0530

    Generate CSV added

--- lib/xls/file.rb
+++ lib/xls/file.rb
@@ -23,6 +23,27 @@ module Xls
     def self.formatted_excel_data(row)
       row.collect{|k, v| [v]}.flatten!
     end
+    def self.generate_csv(data, headers)
+      filename = "csv-file-#{Date.today.strftime('%dth %b %Y')}.csv"
+      path = "tmp/#{filename}"
+
+      CSV.open(path, "w", write_headers: true, headers: headers) do |csv|
+        unless data.is_a? Array
+          data = data.as_json
+        end
+
+        data.each do |row|
+          formatted_row = formatted_csv_data(row)
+          csv << formatted_row
+        end
+      end
+
+      return path
+    end
+
+    def self.formatted_csv_data(row)
+      row.collect { |k, v| v }
+    end
     def self.test
       puts "Cheers!! xls-file installed successfully"
     end
